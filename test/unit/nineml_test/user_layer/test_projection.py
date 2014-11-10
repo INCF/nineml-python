ab74fb4e45168be21001ca76cf5ac7577f856abf
Tom Close <tclose@oist.jp>
2014-11-10 15:57:07 +0900

    Added unit-test for Projections, including simple example (for which I had to update fix the ports of the HH class and add a Response type)

---
+++ test/unit/nineml_test/user_layer/test_projection.py
@@ -0,0 +1,13 @@
+import os.path
+import unittest
+from nineml import read
+from nineml.user_layer import Projection
+
+
+class TestPopulation(unittest.TestCase):
+
+    def test_load(self):
+        context = read(os.path.join(os.path.dirname(__file__), '..', '..',
+                                    '..', '..', '..', '..', 'examples', 'xml',
+                                    'populations',  'simple.xml'))
+        self.assertEquals(type(context['HHPopulation']), Projection)
