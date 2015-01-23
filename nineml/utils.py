021924f62cab0aff908590b39aa04f5726416637
Tom Close <tclose@oist.jp>
2015-01-23 15:09:56 +0900

    consolidated all python examples into a single folder

--- nineml/utils.py
+++ nineml/utils.py
@@ -386,8 +386,8 @@ class LocationMgr(object):
     @classmethod
     def getComponentDir(cls):
         # localDir = realpath ( dirname( __file__ ) )
-        return join_norm(cls.getPythonPackageRootDir(), '..', 'examples',
-                         'AL', 'sample_components')
+        return join_norm(cls.getPythonPackageRootDir(), '..', '..', '..',
+                         'examples', 'python', 'AL', 'sample_components')
 
     @classmethod
     def getTmpDir(cls):
