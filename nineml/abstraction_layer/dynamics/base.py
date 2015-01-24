2415932f76b1347119699f5ddd5db1d480cedc9d
Tom Close <tclose@oist.jp>
2015-01-24 09:13:29 +0900

    added constants to Dynamics class

--- nineml/abstraction_layer/dynamics/base.py
+++ nineml/abstraction_layer/dynamics/base.py
@@ -33,7 +33,8 @@ class DynamicsBlock(BaseALObject):
 
     defining_attributes = ('_regimes', '_aliases', '_state_variables')
 
-    def __init__(self, regimes=None, aliases=None, state_variables=None):
+    def __init__(self, regimes=None, aliases=None, state_variables=None,
+                 constants=None):
         """DynamicsBlock object constructor
 
            :param aliases: A list of aliases, which must be either |Alias|
@@ -69,15 +70,18 @@ class DynamicsBlock(BaseALObject):
         self._regimes = dict((r.name, r) for r in regimes)
         self._aliases = dict((a.lhs, a) for a in aliases)
         self._state_variables = dict((s.name, s) for s in state_variables)
+        self._constants = dict((c.name, c) for c in constants)
 
     def accept_visitor(self, visitor, **kwargs):
         """ |VISITATION| """
         return visitor.visit_dynamicsblock(self, **kwargs)
 
     def __repr__(self):
-        return ('DynamicsBlock({} regimes, {} aliases, {} state-variables)'
+        return ('DynamicsBlock({} regimes, {} aliases, {} state-variables, '
+                '{} constants)'
                 .format(len(list(self.regimes)), len(list(self.aliases)),
-                        len(list(self.state_variables))))
+                        len(list(self.state_variables)),
+                        len(list(self.constants))))
 
     @property
     def regimes(self):
@@ -251,7 +255,8 @@ class DynamicsClass(ComponentClass, _NamespaceMixin):
                  event_ports=[],
                  dynamicsblock=None, subnodes=None,
                  portconnections=None, regimes=None,
-                 aliases=None, state_variables=None):
+                 aliases=None, state_variables=None,
+                 constants=None):
         """Constructs a DynamicsClass
 
         :param name: The name of the componentclass.
@@ -292,13 +297,14 @@ class DynamicsClass(ComponentClass, _NamespaceMixin):
         # We can specify in the componentclass, and they will get forwarded to
         # the dynamics class. We check that we do not specify half-and-half:
         if dynamicsblock is not None:
-            if regimes or aliases or state_variables:
+            if regimes or aliases or state_variables or constants:
                 err = "Either specify a 'dynamicsblock' parameter, or "
                 err += "state_variables /regimes/aliases, but not both!"
                 raise NineMLRuntimeError(err)
         else:
             dynamicsblock = DynamicsBlock(regimes=regimes, aliases=aliases,
-                                          state_variables=state_variables)
+                                          state_variables=state_variables,
+                                          constants=constants)
         ComponentClass.__init__(self, name, parameters,
                                 main_block=dynamicsblock)
         self._query = DynamicsQueryer(self)
