tclose/lib9ML

added rudimentary dependencies retrieval from component classes

nineml/abstraction_layer/dynamics/base.py
@@ -646,6 +646,10 @@ class DynamicsClass(ComponentClass, _NamespaceMixin):
                 originalname=alias.lhs, targetname=("(%s)" % alias.rhs))
             alias_expander.visit(self)
 
+    @property
+    def _assumed_defined(self):
+        return self.dynamics._state_variables.keys()
+
 
 def inf_check(l1, l2, desc):
     check_list_contain_same_items(l1, l2, desc1='Declared',

INCF/nineml-python

nineml/abstraction_layer/dynamics/base.py
@@ -634,6 +634,10 @@ class DynamicsClass(ComponentClass, _NamespaceMixin):
                 originalname=alias.lhs, targetname=("(%s)" % alias.rhs))
             alias_expander.visit(self)
 
+    @property
+    def _assumed_defined(self):
+        return self.dynamicsblock._state_variables.keys()
+
 
 def inf_check(l1, l2, desc):
     check_list_contain_same_items(l1, l2, desc1='Declared',