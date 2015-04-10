tclose/lib9ML

added required definition visitor

nineml/abstraction_layer/connectionrule/base.py
@@ -47,5 +47,9 @@ class ConnectionRuleClass(ComponentClass):
     def rename(self, old_symbol, new_symbol):
         ConnectionRuleRenameSymbol(self, old_symbol, new_symbol)
 
+    def required_definitions(self, expressions):
+        return ConnectionRuleRequiredDefinitions(self, expressions)
+
 from .utils.cloner import ConnectionRuleCloner
 from .utils.modifiers import ConnectionRuleRenameSymbol
+from .utils.visitors import ConnectionRuleRequiredDefinitions

nineml/abstraction_layer/distribution/base.py
@@ -32,5 +32,9 @@ class DistributionClass(ComponentClass):
     def rename(self, old_symbol, new_symbol):
         DistributionRenameSymbol(self, old_symbol, new_symbol)
 
+    def required_definitions(self, expressions):
+        return DistributionRequiredDefinitions(self, expressions)
+
 from .utils.cloner import DistributionCloner
 from .utils.modifiers import DistributionRenameSymbol
+from .utils.visitors import DistributionRequiredDefinitions

INCF/nineml-python

nineml/abstraction_layer/connectionrule/base.py
@@ -47,5 +47,9 @@ class ConnectionRuleClass(ComponentClass):
     def rename(self, old_symbol, new_symbol):
         ConnectionRuleRenameSymbol(self, old_symbol, new_symbol)
 
+    def required_for(self, expressions):
+        return ConnectionRuleRequiredDefinitions(self, expressions)
+
 from .utils.cloner import ConnectionRuleCloner
 from .utils.modifiers import ConnectionRuleRenameSymbol
+from .utils.visitors import ConnectionRuleRequiredDefinitions

nineml/abstraction_layer/distribution/base.py
@@ -32,5 +32,9 @@ class DistributionClass(ComponentClass):
     def rename(self, old_symbol, new_symbol):
         DistributionRenameSymbol(self, old_symbol, new_symbol)
 
+    def required_for(self, expressions):
+        return DistributionRequiredDefinitions(self, expressions)
+
 from .utils.cloner import DistributionCloner
 from .utils.modifiers import DistributionRenameSymbol
+from .utils.visitors import DistributionRequiredDefinitions