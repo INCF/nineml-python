e6f8faba2aa4943974476a6f657b317c73bf2b3c
Tom Close <tclose@oist.jp>
2015-04-14 16:30:34 +0900

    debugging dimensionality checker

--- nineml/abstraction_layer/dynamics/validators/general.py
+++ nineml/abstraction_layer/dynamics/validators/general.py
@@ -231,14 +231,14 @@ class DimensionalityDynamicsValidator(DimensionalityComponentValidator,
                                       PerNamespaceDynamicsValidator):
 
     def action_timederivative(self, timederivative, **kwargs):  # @UnusedVariable @IgnorePep8
-        dimension = self._get_dimension(timederivative)
+        dimension = self._get_dimensions(timederivative)
         self._check_dimesions_are_consistent(dimension)
         self._compare_dimensionality(dimension, timederivative,
                                      self.componentclass.state_variable(
                                          timederivative.variable))
 
     def action_stateassignment(self, stateassignment, **kwargs):  # @UnusedVariable @IgnorePep8
-        dimension = self._get_dimension(stateassignment)
+        dimension = self._get_dimensions(stateassignment)
         self._check_dimesions_are_consistent(dimension)
         self._compare_dimensionality(dimension, stateassignment,
                                      self.componentclass.state_variable(
