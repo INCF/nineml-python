eb1d7e235bc1d1018130e46500aef3d5a562d81f
Tom Close <tclose@oist.jp>
2015-04-15 18:27:56 +0900

    added unit tests for dimension checking

--- test/unit/nineml_test/abstraction_layer/validators/dimensionality_test.py
+++ test/unit/nineml_test/abstraction_layer/validators/dimensionality_test.py
@@ -1,14 +1,16 @@
 from __future__ import division
 import unittest
 from nineml.abstraction_layer import (
-    Parameter, Constant, DynamicsClass, Regime, On, OutputEvent, StateVariable)
+    Parameter, Constant, DynamicsClass, Regime, On, OutputEvent, StateVariable,
+    StateAssignment)
 from nineml.abstraction_layer.ports import AnalogSendPort, AnalogReceivePort
 from nineml import units as un
+from nineml.exceptions import NineMLRuntimeError
 
 
 class Dimensionality_test(unittest.TestCase):
 
-    def test_after_cloning(self):
+    def test_good_model(self):
 
         DynamicsClass(
             name='A',
@@ -37,4 +39,99 @@ class Dimensionality_test(unittest.TestCase):
                         Parameter('P2', dimension=un.time),
                         Parameter('P3', dimension=un.voltage)],
             constants=[Constant('C1', value=1.0, units=un.mV)]
+        )
+
+    def test_internally_inconsistent(self):
+
+        self.assertRaises(
+            NineMLRuntimeError,
+            DynamicsClass,
+            name='A',
+            state_variables=[
+                StateVariable('SV1', dimension=un.voltage)],
+            regimes=[
+                Regime(
+                    'dSV1/dt = SV1 + P1',
+                    name='R1'
+                ),
+            ],
+            parameters=[Parameter('P1', dimension=un.time)],
+        )
+        self.assertRaises(
+            NineMLRuntimeError,
+            DynamicsClass,
+            name='A',
+            state_variables=[
+                StateVariable('SV1', dimension=un.voltage)],
+            regimes=[
+                Regime(
+                    'dSV1/dt = SV1/t',
+                    transitions=[On('SV1 > P1', do=[OutputEvent('emit')])],
+                    name='R1'
+                ),
+            ],
+            parameters=[Parameter('P1', dimension=un.time)],
+        )
+        self.assertRaises(
+            NineMLRuntimeError,
+            DynamicsClass,
+            name='A',
+            state_variables=[
+                StateVariable('SV1', dimension=un.voltage)],
+            regimes=[
+                Regime(
+                    'dSV1/dt = SV1/t',
+                    transitions=[On('SV1 > P1',
+                                    do=[StateAssignment('SV1', 'SV1 + RP1')])],
+                    name='R1'
+                ),
+            ],
+            parameters=[Parameter('P1', dimension=un.voltage)],
+            analog_ports=[AnalogReceivePort('RP1', dimension=un.time)],
+        )
+
+    def test_mismatch_with_declared(self):
+
+        self.assertRaises(
+            NineMLRuntimeError,
+            DynamicsClass,
+            name='A',
+            state_variables=[StateVariable('SV1', dimension=un.voltage)],
+            regimes=[
+                Regime(
+                    'dSV1/dt = SV1/t',
+                    name='R1'
+                ),
+            ],
+            analog_ports=[AnalogSendPort('SV1', dimension=un.current)],
+            parameters=[Parameter('P1', dimension=un.voltage)],
+        )
+        self.assertRaises(
+            NineMLRuntimeError,
+            DynamicsClass,
+            name='A',
+            state_variables=[StateVariable('SV1', dimension=un.voltage)],
+            regimes=[
+                Regime(
+                    'dSV1/dt = SV1 * P1',
+                    name='R1'
+                ),
+            ],
+            parameters=[Parameter('P1', dimension=un.time)],
+        )
+        self.assertRaises(
+            NineMLRuntimeError,
+            DynamicsClass,
+            name='A',
+            state_variables=[StateVariable('SV1', dimension=un.voltage)],
+            regimes=[
+                Regime(
+                    'dSV1/dt = SV1/t',
+                    transitions=[On('SV1 > P1',
+                                    do=[StateAssignment('SV1', 'P2')])],
+                    name='R1'
+                ),
+            ],
+            parameters=[Parameter('P1', dimension=un.voltage),
+                        Parameter('P2', dimension=un.time)],
         )
