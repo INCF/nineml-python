f4fa28ab46c7d3fb8a1d6f33c90e01590e519aca
Tom Close <tclose@oist.jp>
2015-06-03 10:09:58 +0900

    deleted old test directory

--- nineml/abstraction/dynamics/testing_utils/record_value.py
+++
@@ -1,9 +0,0 @@
-"""
-docstring needed
-
-:copyright: Copyright 2010-2013 by the Python lib9ML team, see AUTHORS.
-:license: BSD-3, see LICENSE for details.
-"""
-
-from collections import namedtuple
-RecordValue = namedtuple('Record', ['what', 'tag', 'label'])
