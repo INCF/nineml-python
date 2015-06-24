bd34373e32a266c7361b01f9c6cdcac76e07f34d
Tom Close <tclose@oist.jp>
2015-06-24 10:09:19 +0900

    debugged most of multi-component and multi-compartment xml read/write, just need to resolve circular import problems

--- nineml/user/__init__.py
+++ nineml/user/__init__.py
@@ -1,45 +1,4 @@
 # encoding: utf-8
-"""
-Python module for reading/writing 9ML user layer files in XML format.
-
-Functions
----------
-
-    parse - read a 9ML file in XML format and parse it into a Model instance.
-
-Classes
--------
-    Model
-    Definition
-    Component
-        SpikingNodeType
-        SynapseType
-        CurrentSourceType
-        Structure
-        ConnectionRule
-        ConnectionType
-        RandomDistribution
-    Parameter
-    PropertySet
-    Value
-    Network
-    Population
-    PositionList
-    Projection
-    Selection
-    Operator
-        Any
-        All
-        Not
-        Comparison
-        Eq
-        In
-
-
-:copyright: Copyright 2010-2013 by the Python lib9ML team, see AUTHORS.
-:license: BSD-3, see LICENSE for details.
-
-"""
 from .base import BaseULObject
 from .network import Network
 from .population import Population, PositionList, Structure
