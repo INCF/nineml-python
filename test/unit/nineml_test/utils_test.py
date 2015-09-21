a8006acae7ad4526ccb9d07ce168cfd037a3bc0f
Tom Close <tclose@oist.jp>
2015-09-22 00:03:55 +1000

    renamed user and abstraction layer directories in unit tests

--- test/unit/nineml_test/utils_test.py
+++ test/unit/nineml_test/utils_test.py
@@ -20,7 +20,6 @@ class Testcheck_list_contain_same_items(unittest.TestCase):
         # Signature: name(lst1, lst2, desc1='', desc2='', ignore=[], desc='')
                 # No Docstring
 
-
         self.assertRaises(
             NineMLRuntimeError,
             check_list_contain_same_items,
@@ -63,24 +62,24 @@ class Testfilter_expect_single(unittest.TestCase):
 
     def test_filter_expect_single(self):
         # Signature: name(lst, func=None, error_func=None)
-                # Find a single element matching a predicate in a list.
-                #
-                # This is a syntactic-sugar function ``_filter`` and ``expect_single``
-                # in a single call.
-                #
-                #  Returns::
-                #
-                #      expect_single( _filter(lst, func), error_func )
-                #
-                #
-                #  This is useful when we want to find an item in a sequence with a certain
-                #  property, and expect there to be only one.
-                #
-                #  Examples:
-                #
-                #  >>> find_smith = lambda s: s.split()[-1] == 'Smith'
-                # >>> filter_expect_single( ['John Smith','Tim Jones'], func=find_smith )  #doctest: +NORMALIZE_WHITESPACE
-                #  'John Smith'
+        # Find a single element matching a predicate in a list.
+        #
+        # This is a syntactic-sugar function ``_filter`` and ``expect_single``
+        # in a single call.
+        #
+        #  Returns::
+        #
+        #      expect_single( _filter(lst, func), error_func )
+        #
+        #
+        #  This is useful when we want to find an item in a sequence with a certain
+        #  property, and expect there to be only one.
+        #
+        #  Examples:
+        #
+        #  >>> find_smith = lambda s: s.split()[-1] == 'Smith'
+        # >>> filter_expect_single( ['John Smith','Tim Jones'], func=find_smith )  #doctest: +NORMALIZE_WHITESPACE
+        #  'John Smith'
 
         find_smith = lambda s: s.split()[-1] == 'Smith'
         self.assertEqual(
@@ -156,41 +155,41 @@ class Testexpect_single(unittest.TestCase):
 
     def test_expect_single(self):
         # Signature: name(lst, error_func=None)
-                # Retrieve a single element from an iterable.
-                #
-                # This function tests whether an iterable contains just a single element and
-                # if so returns that element. Otherwise it raises an Exception.
-                #
-                # :param lst: An iterable
-                #
-                # :param error_func: An exception object or a callable. ``error_func`` will be
-                #     raised or called in case there is not exactly one element in ``lst``. If
-                #     ``error_func`` is ``None``, a ``NineMLRuntimeError`` exception will be
-                #     raised.
-                #
-                #
-                # :rtype: the element in the list, ``lst[0]``, provided ``len(lst)==1``
-                #
-                #
-                #
-                # >>> expect_single( ['hello'] )
-                # 'hello'
-                #
-                # >>> expect_single( [1] )
-                # 1
-                #
-                # >>> expect_single( [] ) #doctest: +SKIP
-                # NineMLRuntimeError: expect_single() recieved an iterable of length: 0
-                #
-                # >>> expect_single( [None,None] ) #doctest: +SKIP
-                # NineMLRuntimeError: expect_single() recieved an iterable of length: 2
-                #
-                # >>> expect_single( [], lambda: raise_exception( RuntimeError('Aggh') ) #doctest: +SKIP
-                # RuntimeError: Aggh
-                #
-                # >>> #Slightly more tersly:
-                # >>> expect_single( [], RuntimeError('Aggh') ) #doctest: +SKIP
-                # RuntimeError: Aggh
+        # Retrieve a single element from an iterable.
+        #
+        # This function tests whether an iterable contains just a single element and
+        # if so returns that element. Otherwise it raises an Exception.
+        #
+        # :param lst: An iterable
+        #
+        # :param error_func: An exception object or a callable. ``error_func`` will be
+        #     raised or called in case there is not exactly one element in ``lst``. If
+        #     ``error_func`` is ``None``, a ``NineMLRuntimeError`` exception will be
+        #     raised.
+        #
+        #
+        # :rtype: the element in the list, ``lst[0]``, provided ``len(lst)==1``
+        #
+        #
+        #
+        # >>> expect_single( ['hello'] )
+        # 'hello'
+        #
+        # >>> expect_single( [1] )
+        # 1
+        #
+        # >>> expect_single( [] ) #doctest: +SKIP
+        # NineMLRuntimeError: expect_single() recieved an iterable of length: 0
+        #
+        # >>> expect_single( [None,None] ) #doctest: +SKIP
+        # NineMLRuntimeError: expect_single() recieved an iterable of length: 2
+        #
+        # >>> expect_single( [], lambda: raise_exception( RuntimeError('Aggh') ) #doctest: +SKIP
+        # RuntimeError: Aggh
+        #
+        # >>> #Slightly more tersly:
+        # >>> expect_single( [], RuntimeError('Aggh') ) #doctest: +SKIP
+        # RuntimeError: Aggh
 
 
         # Empty Objects should raise:
@@ -344,25 +343,14 @@ class Testassert_no_duplicates(unittest.TestCase):
 
 
 # Testing Skeleton for function:
-class Testfile_sha1_hexdigest(unittest.TestCase):
-
-    def test_file_sha1_hexdigest(self):
-        # Signature: name(filename)
-                # Returns the SHA1 hex-digest of a file
-        # from nineml.utility import file_sha1_hexdigest
-#         warnings.warn('Tests not implemented: sha1_hex_digest')
-        pass
-
-
-# Testing Skeleton for function:
 class Testfilter_by_type(unittest.TestCase):
 
     def test_filter_by_type(self):
         # Signature: name(lst, acceptedtype)
-                # Find all the objects of a certain type in a list
-                #
-                # This is a syntactic sugar function, which returns a list of all the
-                # objects in a iterable for which  ``isinstance(o,acceptedtype) == True``
+        # Find all the objects of a certain type in a list
+        #
+        # This is a syntactic sugar function, which returns a list of all the
+        # objects in a iterable for which  ``isinstance(o,acceptedtype) == True``
 
         # Good Case
         data = ['hello', 'world', 1, 2, 3, None]
@@ -392,19 +380,19 @@ class Testfilter_discrete_types(unittest.TestCase):
 
     def test_filter_discrete_types(self):
         # Signature: name(lst, acceptedtypes)
-                # Creates a dictionary mapping types to objects of that type.
-                #
-                # Starting with a list of object, and a list of types, this returns a
-                # dictionary mapping each type to a list of objects of that type.
-                #
-                # For example::
-                #
-                #     >>> import types
-                # >>> filter_discrete_types( ['hello',1,2,'world'], ( basestring, types.IntType) ) #doctest: +NORMALIZE_WHITESPACE
-                #     {<type 'basestring'>: ['hello', 'world'], <type 'int'>: [1, 2]}
-                #
-                #
-                # The function checks that each object is mapped to exactly one type
+        # Creates a dictionary mapping types to objects of that type.
+        #
+        # Starting with a list of object, and a list of types, this returns a
+        # dictionary mapping each type to a list of objects of that type.
+        #
+        # For example::
+        #
+        #     >>> import types
+        # >>> filter_discrete_types( ['hello',1,2,'world'], ( basestring, types.IntType) ) #doctest: +NORMALIZE_WHITESPACE
+        #     {<type 'basestring'>: ['hello', 'world'], <type 'int'>: [1, 2]}
+        #
+        #
+        # The function checks that each object is mapped to exactly one type
 
         # Good Case:
         data = ['hello', 'world', 1, 2, 3]
@@ -426,9 +414,9 @@ class Testrestore_sys_path(unittest.TestCase):
 
     def test_restore_sys_path(self):
         # Signature: name(func)
-                # Decorator used to restore the sys.path
-                # to the value it was before the function call.
-                # This is useful for loading modules.
+        # Decorator used to restore the sys.path
+        # to the value it was before the function call.
+        # This is useful for loading modules.
 
         original_path = sys.path[:]
 
@@ -457,8 +445,8 @@ class Testsafe_dict(unittest.TestCase):
 
     def test_safe_dict(self):
         # Signature: name(vals)
-                # Create a dict, like dict(), but ensure no duplicate keys are given!
-                # [Python silently allows dict( [(1:True),(1:None)] ) !!
+        # Create a dict, like dict(), but ensure no duplicate keys are given!
+        # [Python silently allows dict( [(1:True),(1:None)] ) !!
 
 
         self.assertEqual(
@@ -482,16 +470,16 @@ class Testsafe_dictionary_merge(unittest.TestCase):
 
     def test_safe_dictionary_merge(self):
         # Signature: name(dictionaries)
-                # Safely merge multiple dictionaries into one
-                #
-                # Merges an iterable of dictionaries into a new single dictionary,
-                # checking that there are no key collisions
-                #
-                # >>> safe_dictionary_merge( [ {1:'One',2:'Two'},{3:'Three'} ] ) #doctest: +NORMALIZE_WHITESPACE
-                # {1: 'One', 2: 'Two', 3: 'Three'}
-                #
-                # >>> safe_dictionary_merge( [ {1:'One',2:'Two'},{3:'Three',1:'One'} ] ) #doctest: +NORMALIZE_WHITESPACE +IGNORE_EXCEPTION_DETAIL +SKIP
-                # NineMLRuntimeError: Key Collision while merging dictionarys
+        # Safely merge multiple dictionaries into one
+        #
+        # Merges an iterable of dictionaries into a new single dictionary,
+        # checking that there are no key collisions
+        #
+        # >>> safe_dictionary_merge( [ {1:'One',2:'Two'},{3:'Three'} ] ) #doctest: +NORMALIZE_WHITESPACE
+        # {1: 'One', 2: 'Two', 3: 'Three'}
+        #
+        # >>> safe_dictionary_merge( [ {1:'One',2:'Two'},{3:'Three',1:'One'} ] ) #doctest: +NORMALIZE_WHITESPACE +IGNORE_EXCEPTION_DETAIL +SKIP
+        # NineMLRuntimeError: Key Collision while merging dictionarys
 
         self.assertEqual(
             safe_dictionary_merge([{1: 'One'}, {2: 'Two'}]),
@@ -507,41 +495,3 @@ class Testsafe_dictionary_merge(unittest.TestCase):
             safe_dictionary_merge,
             [{1: 'One'}, {2: 'Two', 3: 'Three', 1: 'One'}, {4: 'Four'}]
         )
-
-
-# Testing Skeleton for function:
-class Test_filter(unittest.TestCase):
-
-    def test__filter(self):
-        # Signature: name(lst, func=None)
-                # Filter a list according to a predicate.
-                #
-                # Takes a sequence [o1,o2,..] and returns a list contains those which
-                # are not `None` and satisfy the predicate `func(o)`
-                #
-                # :param lst: Input iterable
-                # :param func: Predicate function. If ``none``, this function always returns ``True``
-                #
-                #
-                # Implementation::
-                #
-                #     if func:
-                #         return  [ l for l in lst if l is not None and func(l) ]
-                #     else:
-                #         return  [ l for l in lst if l is not None]
-                #
-                # Examples:
-                #
-                # >>> _filter( ['hello','world'] )         #doctest: +NORMALIZE_WHITESPACE
-                # ['hello', 'world']
-                #
-                #
-                # >>> _filter( ['hello',None,'world'] )    #doctest: +NORMALIZE_WHITESPACE
-                # ['hello', 'world']
-                #
-                # >>> _filter( [None,] )                   #doctest: +NORMALIZE_WHITESPACE
-                # []
-        # from nineml.utility import _filter
-#         warnings.warn('Tests not implemented: _filter')
-        # raise NotImplementedError()
-        pass
