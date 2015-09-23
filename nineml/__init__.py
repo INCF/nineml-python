8c8e53cebd2e1203acb5713b5b4ae4f8986523cf
Tom Close <tclose@oist.jp>
2015-09-23 22:52:54 +1000

    cleaned up MultiDynamicsProperties __init__ method

--- nineml/__init__.py
+++ nineml/__init__.py
@@ -20,4 +20,4 @@ from abstraction import (
 from .user import (
     Selection, Population, Projection, Property, Definition, Component,
     DynamicsProperties, ConnectionRuleProperties, RandomDistributionProperties,
-    Network, MultiDynamics, MultiCompartment)
+    Network, MultiDynamics)
