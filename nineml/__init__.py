c787ae041fa6f70448ef72735aa32785919596bc
Tom Close <tclose@oist.jp>
2016-07-31 21:41:47 +1000

    imported missing classes into root package

--- nineml/__init__.py
+++ nineml/__init__.py
@@ -10,12 +10,13 @@ from . import abstraction
 from . import user
 from . import exceptions
 from . import units
-from .units import Unit, Dimension
+from .units import Unit, Dimension, Quantity
 from abstraction import (
     Dynamics, ConnectionRule, RandomDistribution)
 from .user import (
     Selection, Population, Projection, Property, Definition,
     DynamicsProperties, ConnectionRuleProperties, RandomDistributionProperties,
     Network, MultiDynamics, MultiDynamicsProperties, Concatenate)
+from .values import SingleValue, ArrayValue, RandomValue
 
 __version__ = "0.1"
