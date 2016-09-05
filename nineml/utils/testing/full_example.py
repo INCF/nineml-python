5c0a251da937e77459b05669c2f0a232452b3f32
Tom Close <tclose@oist.jp>
2016-09-05 22:00:29 +1000

    adding more elements to full example

--- nineml/utils/testing/full_example.py
+++ nineml/utils/testing/full_example.py
@@ -1,14 +1,14 @@
 import nineml.units as un
 from nineml.document import Document
 from nineml.abstraction import (
-    Parameter, Constant, Dynamics, Regime,
+    Parameter, Constant, Dynamics, Regime, Alias,
     OutputEvent, StateVariable, StateAssignment, On, AnalogSendPort,
     AnalogReceivePort, ConnectionRule, RandomDistribution)
 from nineml.user import (
     Population, Selection, Concatenate, Projection, Property, Component,
-    Definition, Prototype, resolve_reference, write_reference, Initial,
+    Definition, Prototype, Initial,
     DynamicsProperties, ConnectionRuleProperties, RandomDistributionProperties,
-    Quantity, MultiDynamics, MultiDynamicsProperties, AnalogPortConnection,
+    MultiDynamics, MultiDynamicsProperties, AnalogPortConnection,
     EventPortConnection, Network, ComponentArray, AnalogConnectionGroup,
     EventConnectionGroup)
 
@@ -70,6 +70,26 @@ dynB = Dynamics(
     parameters=['P1', 'P2', 'P3']
 )
 
+
+dynC = Dynamics(
+    name='dynC',
+    aliases=['A1:=P1', 'A2 := ARP1 + SV2', 'A3 := SV1'],
+    regimes=[
+        Regime(
+            'dSV1/dt = -SV1 / (P2*t)',
+            'dSV2/dt = SV1 / (ARP1*t) + SV2 / (P1*t)',
+            transitions=[On('SV1 > P1', do=[OutputEvent('emit')]),
+                         On('spikein', do=[OutputEvent('emit')])],
+            aliases=[Alias('A1', 'P1 * 2')],
+            name='R1',
+        ),
+        Regime(name='R2', transitions=On('SV1 > 1', to='R1'))
+    ],
+    analog_ports=[AnalogReceivePort('ARP1'), AnalogReceivePort('ARP2'),
+                  AnalogSendPort('A1'), AnalogSendPort('A2')],
+    parameters=['P1', 'P2']
+)
+
 dynPropA = DynamicsProperties(
     name='dynA',
     definition=dynA,
@@ -87,29 +107,47 @@ dynPropB = DynamicsProperties(
         'P2': 1.0 * un.unitless,
         'P3': 1.0 * un.unitless})
 
-ConA = ConnectionRule(
+dynPropC = DynamicsProperties(
+    name='dynCProp',
+    definition=dynC,
+    properties={
+        'P1': 1.0 * un.unitless,
+        'P2': 1.0 * un.unitless})
+
+conA = ConnectionRule(
     name="ConA",
     standard_library='http://nineml.net/9ML/1.0/connectionrules/RandomFanIn',
     parameters=[Parameter('P1', dimension=un.dimensionless)])
 
-ConPropA = ConnectionRuleProperties(
+conPropA = ConnectionRuleProperties(
     name="ConPropA",
-    definition=ConA,
+    definition=conA,
     properties={'P1': 1.0 * un.unitless})
 
-RanDistrA = RandomDistribution(
+ranDistrA = RandomDistribution(
     name="RanDistrA",
     standard_library="http://www.uncertml.org/distributions/exponential",
     parameters=[Parameter('P1', dimension=un.dimensionless)])
 
-RanDistrPropA = RandomDistributionProperties(
+ranDistrPropA = RandomDistributionProperties(
     name="RanDistrPropA",
-    definition=RanDistrA,
+    definition=ranDistrA,
     properties={'P1': 1.0 * un.unitless})
 
-PopA = Population(
-    name="PopA",
+popA = Population(
+    name="popA",
     size=10,
     cell=dynPropA)
 
-doc = Document((dynA, PopA))
+popB = Population(
+    name='popB',
+    size=100,
+    cell=dynPropB)
+
+popC = Population(
+    name='popC',
+    size=50,
+    cell=dynPropC)
+
+doc = Document((dynA, dynB, dynC, dynPropA, dynPropB, dynPropC, conA,
+                conPropA, ranDistrA, ranDistrPropA, popA, popB, popC))
