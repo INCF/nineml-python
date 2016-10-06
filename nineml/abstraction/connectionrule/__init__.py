43b63f48ee388591af7eba89d9511674cb87fa43
Tom Close <tclose@oist.jp>
2016-10-06 21:46:09 +1100

    moved connectivity into connectionrule module

--- nineml/abstraction/connectionrule/__init__.py
+++ nineml/abstraction/connectionrule/__init__.py
@@ -1,4 +1,5 @@
 from .base import (
     ConnectionRule, one_to_one_connection_rule, explicit_connection_rule,
-    probabilistic_rule, random_fan_in_rule, random_fan_out_rule)
+    probabilistic_rule, random_fan_in_rule, random_fan_out_rule,
+    Connectivity, InverseConnectivity)
 from .visitors import ConnectionRuleXMLLoader, ConnectionRuleXMLWriter
