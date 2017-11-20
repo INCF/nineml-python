0f967c897cc6d164dd62fdfffb4e8c2e37e8f101
Tom Close <tom.g.close@gmail.com>
2017-11-20 23:11:07 +1100

    upgraded version to 1.0rc2 (mainly to get PyPI to render properly)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="nineml",
-    version="1.0rc1",
+    version="1.0rc2",
     packages=find_packages(),
     # add your name here if you contribute to the code
     author="Andrew P. Davison, Thomas G. Close, Mike Hull, Eilif Muller",
