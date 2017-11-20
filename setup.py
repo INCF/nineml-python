f509be3673b7b2d86e6b7f2283c5db7b1f874cd9
Tom Close <tom.g.close@gmail.com>
2017-11-21 10:03:44 +1100

    updated PyYAML dependency

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     install_requires=['lxml>=3.7.3',
                       'future>=0.16.0',
                       'h5py>=2.7.0',
-                      'pyyaml>=3.1',
+                      'PyYAML>=3.1',
                       'sympy>=1.1'],
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4',
     tests_require=['nose', 'numpy']
