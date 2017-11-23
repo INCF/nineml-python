65937a7f9f18b6357905bc4874d32f005bf22570
Tom Close <tom.g.close@gmail.com>
2017-11-23 22:32:39 +1100

    added Python 3.3 to available versions as passes unittests

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setup(
                  'Programming Language :: Python :: 2',
                  'Programming Language :: Python :: 2.7',
                  'Programming Language :: Python :: 3',
+                 'Programming Language :: Python :: 3.3',
                  'Programming Language :: Python :: 3.4',
                  'Programming Language :: Python :: 3.5',
                  'Programming Language :: Python :: 3.6',
@@ -35,6 +36,6 @@ setup(
                       'h5py>=2.7.0',
                       'PyYAML>=3.1',
                       'sympy>=1.1'],
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4',
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, <4',
     tests_require=['nose', 'numpy']
 )
