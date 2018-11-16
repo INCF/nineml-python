6ce9cfd2c4e7547c8a126bdab6b9a7d14daf1353
Tom Close <tom.g.close@gmail.com>
2018-11-16 14:23:51 +1100

    added support for Py 3.7

--- setup.py
+++ setup.py
@@ -39,6 +39,7 @@ setup(
                  'Programming Language :: Python :: 3.4',
                  'Programming Language :: Python :: 3.5',
                  'Programming Language :: Python :: 3.6',
+                 'Programming Language :: Python :: 3.7',
                  'Topic :: Scientific/Engineering'],
     install_requires=['lxml>=3.7.3',
                       'future>=0.16.0',
